968438cf82b41d52eb7f7ffda2a4251cc8fa4326
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-10-27 01:15:10 -0400

    Add files via upload

---
+++ CMHealthCheck.psm1
@@ -0,0 +1,43 @@
+#Module vars
+$ModulePath = $PSScriptRoot
+
+# Thank you to https://github.com/ramblingcookiemonster for this module and methodology
+# Get public and private function definition files.
+$Public  = Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue
+$Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue
+[string[]]$PrivateModules = Get-ChildItem $PSScriptRoot\Private -ErrorAction SilentlyContinue |
+    Where-Object {$_.PSIsContainer} |
+    Select-Object -ExpandProperty FullName
+
+# Dot source the files
+if ($Private) {
+    Foreach ($import in @($Public + $Private)) {
+        Try {
+            . $import.fullname
+        }
+        Catch {
+            Write-Error "Failed to import function $($import.fullname): $_"
+        }
+    }
+}
+else {
+    Foreach ($import in $Public) {
+        Try {
+            . $import.fullname
+        }
+        Catch {
+            Write-Error "Failed to import function $($import.fullname): $_"
+        }
+    }
+}
+
+
+# Load up dependency modules
+foreach ($Module in $PrivateModules) {
+    Try {
+        Import-Module $Module -ErrorAction Stop
+    }
+    Catch {
+        Write-Error "Failed to import module $Module`: $_"
+    }
+}
