11887cf45be0164d099584a1f9eb4650d9477fc8
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-12-01 16:11:20 -0500

    Add files via upload

--- CMHealthCheck.psm1
+++ CMHealthCheck.psm1
@@ -1,3 +1,3 @@
-$ScriptVersion = $MyInvocation.MyCommand.Module.Version
+$ScriptVersion = '1.0.3'
 Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private'),(Join-Path -Path $PSScriptRoot -ChildPath 'Public') -Filter '*.ps1' |
     ForEach-Object { . $_.FullName }
