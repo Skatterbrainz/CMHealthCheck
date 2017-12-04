e000c9989afe49c6efed62f06d4b01ef62cd8a00
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-12-04 01:04:25 -0500

    Updated for 1.0.3 version stamp

--- CMHealthCheck.psm1
+++ CMHealthCheck.psm1
@@ -1,3 +1,2 @@
-$ScriptVersion = '1.0.3'
 Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private'),(Join-Path -Path $PSScriptRoot -ChildPath 'Public') -Filter '*.ps1' |
     ForEach-Object { . $_.FullName }
