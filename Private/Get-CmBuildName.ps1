73f7367ded305a8a97713bbfe59149fa1d139ed9
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-09-21 01:55:20 -0400

    Create Get-CmBuildName.ps1

---
+++ Private/Get-CmBuildName.ps1
@@ -0,0 +1,25 @@
+function Get-CmBuildName {
+    param (
+        [parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string] $BuildNumber
+    )
+    $ModuleData = Get-Module CMHealthCheck
+    $ModuleVer  = $ModuleData.Version -join '.'
+    $ModulePath = $ModuleData.Path -replace 'CMHealthCheck.psm1', ''
+    $bdatafile  = "$ModulePath"+"assets\buildnumbers.txt"
+    if (!(Test-Path $bdatafile)) {
+        Write-Error "$bdatafile could not be found or imported"
+        break
+    }
+    $bdata = Get-Content $bdatafile
+    foreach ($row in $bdata) {
+        $bset = $row -split "="
+        $bnum = $bset[0]
+        if ($bnum -eq $BuildNumber) {
+            $result = $bset[1]
+            break
+        }
+    }
+    Write-Output $result
+}
