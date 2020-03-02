08cfbf51e5a72ed9f06018c742ce862e19c64b87
skatterbrainz <dstein2017@gmail.com>
2020-03-02 12:51:07 -0500

    bugfix

--- Private/Get-MessageInformation.ps1
+++ Private/Get-MessageInformation.ps1
@@ -3,39 +3,45 @@ function Get-MessageInformation {
     param (
 		[parameter()][string]$MessageID = ""
 	)
-	if (![string]::IsNullOrEmpty($MessageID)) {
-		Write-Log "looking up information for error messageID = $MessageID" -Log $logfile
-		$msg = $MessagesXML.dtsHealthCheck.Message | Where-Object {$_.MessageId -eq $MessageID}
-		if ($null -eq $msg) {
-			Write-Log "searching windows update error table" -Log $logfile
-			$errcodes = Join-Path $(Split-Path (Get-Module "cmhealthcheck").Path) -ChildPath "assets\windows_update_errorcodes.csv"
-			if (Test-Path $errcodes) {
-				$errdata = Import-Csv -Path $errcodes
-				$errdet = $($errdata | Where-Object {$_.ErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
-				if ([string]::IsNullOrEmpty($errdet)) {
-					$errdet = $($errdata | Where-Object {$_.DecErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
-					if (![string]::IsNullOrEmpty($errdet)) {
-						Write-Output $errdet
+	try {
+		if (![string]::IsNullOrEmpty($MessageID)) {
+			Write-Log "looking up information for error messageID = $MessageID" -LogFile $logfile
+			$msg = $MessagesXML.dtsHealthCheck.Message | Where-Object {$_.MessageId -eq $MessageID}
+			if ($null -eq $msg) {
+				Write-Log "searching windows update error table" -LogFile $logfile
+				$errcodes = Join-Path $(Split-Path (Get-Module "cmhealthcheck").Path) -ChildPath "assets\windows_update_errorcodes.csv"
+				if (Test-Path $errcodes) {
+					Write-Log "importing lookup data" -LogFile $logfile
+					$errdata = Import-Csv -Path $errcodes
+					$errdet = $($errdata | Where-Object {$_.ErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
+					if ([string]::IsNullOrEmpty($errdet)) {
+						$errdet = $($errdata | Where-Object {$_.DecErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
+						if (![string]::IsNullOrEmpty($errdet)) {
+							Write-Output $errdet
+						}
+						else {
+							Write-Output "There is no known possible solution for Message ID $MessageID"
+						}
 					}
 					else {
-						Write-Output "There is no known possible solution for Message ID $MessageID"
+						Write-Output $errdet
 					}
 				}
 				else {
-					Write-Output $errdet
+					Write-Log "file not found: $errcodes" -LogFile $logfile -Severity 3
+					Write-Output "Unknown Message ID $MessageID"
 				}
 			}
 			else {
-				Write-Warning "missing file: $errcodes"
-				Write-Output "Unknown Message ID $MessageID"
+				Write-Output $msg.Description
 			}
 		}
 		else {
-			Write-Output $msg.Description
+			throw "MessageID was blank or null"
 		}
 	}
-	else {
-		Write-Log "MessageID was blank or null" -Log $logfile -Severity 3
+	catch {
+		Write-Log "Error: $($_.Exception.Message -join ';')" -LogFile $logfile -Severity 3
 		Write-Output ""
 	}
 }
--- Private/Get-MessageSolution.ps1
+++ Private/Get-MessageSolution.ps1
@@ -5,19 +5,19 @@ function Get-MessageSolution {
     )
 	try {
 		if (![string]::IsNullOrEmpty($MessageID)) {
-			Write-Log "looking up solution for error message id: $MessageID" -Log $logfile
+			Write-Log "looking up solution for error message id: $MessageID" -LogFile $logfile
 			$msg = $MessagesXML.dtsHealthCheck.MessageSolution | Where-Object {$_.MessageId -eq $MessageID}
 			if ([string]::IsNullOrEmpty($msg)) {
-				Write-Log "searching windows update error solutions table" -Log $logfile
+				Write-Log "searching windows update error solutions table" -LogFile $logfile
 				$errcodes = Join-Path $(Split-Path (Get-Module "cmhealthcheck").Path) -ChildPath "assets\windows_update_errorcodes.csv"
 				if (Test-Path $errcodes) {
-					Write-Log "importing: $errcodes" -Log $logfile
+					Write-Log "importing: $errcodes" -LogFile $logfile
 					$errdata = Import-Csv -Path $errcodes
 					if (![string]::IsNullOrEmpty($errdata)) {
-						Write-Log "imported $($errdata.Count) rows from file" -Log $logfile
+						Write-Log "imported $($errdata.Count) rows from file" -LogFile $logfile
 						$errdet = $($errdata | Where-Object {$_.ErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
 						if ([string]::IsNullOrEmpty($errdet)) {
-							Write-Log "standard details not found. searching decimal error information" -Log $logfile
+							Write-Log "standard details not found. searching decimal error information" -LogFile $logfile
 							$errdet = $($errdata | Where-Object {$_.DecErrorCode -eq $MessageID} | Select-Object -ExpandProperty Description).Trim()
 							if (![string]::IsNullOrEmpty($errdet)) {
 								Write-Output $errdet
@@ -31,7 +31,7 @@ function Get-MessageSolution {
 						}
 					}
 					else {
-						Write-Log "failed to import $errcodes"
+						Write-Log "failed to import $errcodes" -LogFile $logfile
 						Write-Output ""
 					}
 				}
@@ -45,11 +45,11 @@ function Get-MessageSolution {
 			}
 		}
 		else {
-			Write-Log "MessageID was blank or null" -Log $logfile
+			Write-Log "MessageID was blank or null" -LogFile $logfile
 			Write-Output ""
 		}
 	}
 	catch {
-		Write-Log $_.Exception.Message -Log $logfile -Severity 3
+		Write-Log $_.Exception.Message -LogFile $logfile -Severity 3
 	}
 }
